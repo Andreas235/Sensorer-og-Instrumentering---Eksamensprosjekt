7d417893fe927013b1e4683b5bc53c3450c7c68b
Andreas235 <andreas.nysater@ntnu.no>
2025-05-31 15:42:51 +0200

    Organisert

--- plottingScopeD8.py
+++ plottingScopeD8.py
@@ -1,51 +1,26 @@
-# -*- coding: utf-8 -*-
-
-"""
-Created on Sat Apr 15 21:33:57 2023
-
-@author: anyse
-"""
-
 import numpy as np
-#Plotting
 import matplotlib.pyplot as plt
-#Lese CSV
 import csv
 
 data = []
 
 #Henter data fra csvfil
-with open(r'C:\MTELSYS\ESDA2\D8\scopeYtData.csv') as csvfile:
+with open(r'maalingerMotor/motorMaalingMedStoy2.csv') as csvfile:
     csvreader = csv.reader(csvfile)
-
 
     for datapoint in csvreader:
         values = [float(value) for value in datapoint]
         data.append(values)
         
-    #Legger inn data fra hver kanal i hver sin liste
     time = [(p[0]) for p in data]
     ch1 = [(p[1]) for p in data]
-    #ch2 = [(p[2]) for p in data]
-    
 
-
-
-    fig, (ax) = plt.subplots()
-    
-
-    
-    
-    ax.plot(time, ch1, label='$y(t)$', color='orange')
-    # ax.plot(time, ch2, label='$V_0$')    
+    fig, (ax) = plt.subplots()  
+    ax.plot(time, ch1, label='DC-Motor Spenning', color='orange')
     ax.set_xlabel('Tid [s]')
     ax.set_ylabel('Amplitude [V]')
     ax.legend(loc='upper right')
-    ax.axhline(y = 0, color = 'black')
-    # ax.axvline(x = 0.00001, color = 'black')
-    ax.set_title('Utgangssignal $y(t)$')
-    ax.set_xlim(0.02,0.030)
-    # ax.grid()
-    
+    ax.set_title('DC-Motor Spenning med Støy 2')
+    ax.grid()
     # Lagrer figuren
-    fig.savefig('C:\MTELSYS\ESDA2\D8\scopeYt.png', dpi=400)
+    fig.savefig('motorMaalingMedStoy2.png', dpi=400)
